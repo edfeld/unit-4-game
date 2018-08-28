d5a3461c8479dbccc1d4ee13451aa91f4f7bbad0
edfeld <edfeld1@gmail.com>
2018-08-28 10:27:07 -0700

    style adjustments - color positioning

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,44 +1,82 @@
 /* game.js */
 $(document).ready(function() {
 
-    let buttonInput;
+    let buttonColor;
+    let buttonValue = 0;
+    let gameStarted = false;
     
+
+    let theGame = {
+        targetNumber: 0,
+        playerTotal: 0,
+        playerWins: 0,
+        playerLosses: 0,
+        gameStarted: false,
+        arrCrystal : [ "green", "orange", "pink", "purple"],
+        arrCrystalNumber : [ 0, 0, 0, 0],
+
+        resetGame :function() {
+            let randNum = 0;
+            this.gameStarted = true;
+            this.targetNumber = Math.floor((Math.random() * 102) + 19);
+            console.log("23 targetNumber: " + theGame.targetNumber);
+            $(".random-target-num").text(theGame.targetNumber);
+            /* set total score to zero */
+            $(".total-score").text("0");
+            let varCrystal = $(".gem");
+            varCrystal.each(function( index) {
+                randNum = Math.floor((Math.random() * 12) + 1);
+                $( this ).attr( "value", randNum);
+                console.log(this);
+            })
+            // $( "input[value='Hot Fuzz']" )
+            
+            // varCrystal.attr( "value", randNum);
+            // $( "li" ).each(function( index ) {
+            //     console.log( index + ": " + $( this ).text() );
+            //   });
+            // });
+            
+
+        }
+    }
+    
+    if(!gameStarted) {
+        theGame.resetGame();
+    }
 
 
     // Your code here...
     $(".gem").on("click", function() {
-    console.log(this);
-    buttonInput = $(this).attr("color");
-    console.log(buttonInput);
+        console.log(this);
+        buttonColor = $(this).attr("color");
+        buttonValue = $(this).attr("value")
+        console.log(buttonColor);
+        console.log("51. buttonValue", buttonValue);
+        
+        console.log("57. PlayerTotal - before JQuery access: ", theGame.playerTotal);
+        theGame.playerTotal = parseInt($(".total-score").text(), 10);
+        theGame.playerTotal += parseInt(buttonValue, 10);
+        console.log("User total: ", theGame.playerTotal);
+        $(".total-score").text(theGame.playerTotal.toString());
+        if (theGame.playerTotal == theGame.targetNumber) {
+            console.log(" You win!! ");
+            playerWins++;
+            theGame.resetGame();
 
-    
+        }
+        else if(theGame.playerTotal > theGame.targetNumber) {
+            console.log(" You lose")
+            
+            theGame.playerlosses = parseInt($(".losses").text(), 10);
+            theGame.playerlosses++;
 
-    // switch(buttonInput) {
-    //     case "1":
-    //     case "2":
-    //       if (choice1 == "") {
-    //         choice1 = buttonInput;
-    //         console.log("number pressed");
-    //       } else {
-    //         choice2 = buttonInput;
-    //       }
-    //         break;
-    //     case "plus":
-    //      case "subtract":
-    //     case "multiply":
-    //       Operator1 = buttonInput;
-    //     console.log("+ pressed");
-    //         break;
-    //     case "equals":
-    //       if (operator1 == "plus") {
-    //         Result = parseInt(choice1, 10) + parseInt(choice2, 10);
-    //         console.log("result = " + Result);
-    //       }
-    //         break;
-    //     default:
-    //         console.log("Nothing");
-    //     }
-    //     });
+            theGame.resetGame();
+        }
+
+
+
+
 
     });
 
